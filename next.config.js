4dabcafd9e363f07d231086601e82e334c7e8598
nxy7 <lolnoxy@gmail.com>
2023-01-08 17:32:58 +0100

    www moved to out

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  // basePath: "/www",
-  distDir: "/www",
   assetPrefix: isProd ? "/nxy7.github.io/" : "",
-  /* config options here */
 };
 
 module.exports = nextConfig;
