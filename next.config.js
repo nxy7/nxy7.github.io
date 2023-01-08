5167500e705f2c26c72f3d42a495f4df7fb1312a
nxy7 <lolnoxy@gmail.com>
2023-01-08 17:42:45 +0100

    website is not deployed by github actions

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
-const isProd = process.env.NODE_ENV === "production";
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  assetPrefix: isProd ? "/nxy7.github.io/" : "",
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 module.exports = nextConfig;
