f93e04f531c165a0f2a1457d2fc6786dfff4d17c
nxy7 <lolnoxy@gmail.com>
2023-01-18 18:54:18 +0100

    asset paths fix so css is loaded on 'next export'

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = "/";
+let assetPrefix = "./";
 let basePath = "";
 
 if (isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
+  assetPrefix = `./${repo}/`;
+  basePath = `./${repo}`;
 }
 
 /**
