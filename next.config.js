5f353341fc2d79419614892165e1b806083fbcb5
nxy7 <lolnoxy@gmail.com>
2023-01-18 19:08:07 +0100

    fixed lazygit fkup

--- next.config.js
+++ next.config.js
@@ -8,11 +8,7 @@ if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
   assetPrefix = `./${repo}/`;
-<<<<<<< HEAD
-  basePath = `./${repo}`;
-=======
   basePath = `/${repo}`;
->>>>>>> 8e07d0829d35c9b7e84ff5b7eac3b4dfdf7d1f22
 }
 
 /**
