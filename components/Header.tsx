859405adec8771e7d1ae60657cea19104fcbf666
nxy7 <lolnoxy@gmail.com>
2023-01-18 17:41:16 +0100

    first frontend skeleton

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+	return (
+		<div className="fixed flex z-[100] left-6 top-0 h-screen w-10 ">
+			<div className="flex m-auto py-4 px-3 rounded-full flex-col space-y-4 shadow-md items-center justify-center bg-blue-300">
+				<div>1</div>
+				<div>1</div>
+				<div>1</div>
+				<div>1</div>
+			</div>
+		</div>
+	)
+}
