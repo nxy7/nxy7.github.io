0e5106accbed71878e31a09bb187193b0c9a526f
nxyt <lolnoxy@gmail.com>
2023-05-25 02:42:18 +0200

    devenv added

---
+++ devenv.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+
+  packages = with pkgs; [ ];
+
+  # languages.nix.enable = true;
+  languages.typescript.enable = true;
+
+}
--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
   description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flakeUtils.url = "github:numtide/flake-utils";
+    nix2container.url = "github:nlewo/nix2container";
+    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, flakeUtils }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-    in {
-
-      packages.x86_64-linux.hello = pkgs.hello;
-
-      packages.x86_64-linux.default = pkgs.hello;
-
-      devShells.x86_64-linux.default =
-        pkgs.mkShell { packages = with pkgs; [ hello ]; };
-
-    };
+  outputs = { self, nixpkgs, flakeUtils, nix2container, devenv, ... }@inputs:
+    flakeUtils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = devenv.lib.mkShell {
+          inherit pkgs inputs;
+          modules = [ (import ./devenv.nix { inherit pkgs inputs; }) ];
+        };
+      });
 }
