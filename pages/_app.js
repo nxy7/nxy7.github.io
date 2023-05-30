fd398019a019ac65a2a9aec0056c70e80c2666c1
nxyt <lolnoxy@gmail.com>
2023-05-30 19:46:43 +0200

    tracking script fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import "../styles/globals.css"
 import "uno.css";
+import Script from "next/script"
 
 const MyApp = ({ Component, pageProps }) => {
 
   return (
     <>
+      <Script strategy="afterInteractive" async src="https://www.googletagmanager.com/gtag/js?id=G-D4BXHY930J" />
+      <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-D4BXHY930J');
+      `  
+    }}/>
       <Component {...pageProps} />
     </>
   )
